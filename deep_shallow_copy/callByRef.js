55cb61e702d5d897e68960cc831e2597e513faaa
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2024-12-26 04:08:35 +0530

    new

--- deep_shallow_copy/callByRef.js
+++ deep_shallow_copy/callByRef.js
@@ -1,12 +1,22 @@
 //this is the example of call by reference : 
 
-function modifyObject(arr) {
-    arr[1] = 30; // Modifies the original object
-    console.log("Inside function: arr =", arr);
+function modifyObject(obj) {
+    obj.age = 30; // Modifies the original object
+    console.log("Inside function: obj =", obj);
 }
   
-let person = [ "Alice", 25 ];
+let person = { name: "Alice", age: 25 };
 modifyObject(person);
   
 console.log("Outside function: person =", person); // Original object is modified
   
+
+function modifyObject(arr) {
+    arr[1] = 30; // Modifies the original array
+    console.log("Inside function: arr =", arr);
+}
+  
+let stdnt = [ "Alice", 22 ];
+modifyObject(stdnt);
+  
+console.log("Outside function: person =", stdnt); // Original array is modified
