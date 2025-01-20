820984e6404250f86f3268ab492f27ffb2f0d3f3
quasarCoderdilip <dilipkumarrajkumar.1101@gmail.com>
2025-01-20 21:55:38 +0530

    js

--- inter.js
+++ inter.js
@@ -1,3 +1,5 @@
+//1:
+
 // function name_d(){
 //     console.log(name_d.xyz)
 // }
@@ -7,4 +9,28 @@
 // name_d.xyz = 200
 // name_d()
 
+//2: event propogation:
 
+// const numbers = [1,2,3,4,5]
+// numbers[100] = 500
+// console.log(numbers)
+
+
+//3:
+
+// const arr = [...'praveen']
+// console.log(arr)
+
+
+//4:
+
+// console.log(parseInt('10+2'))
+// console.log(parseInt('7FM'))
+// console.log(parseInt('M7F'))
+ 
+//5:
+
+// console.log([0,1,2,3].map(num =>{
+//     if (num > 0) return;
+//     return num * 2;
+// }))
